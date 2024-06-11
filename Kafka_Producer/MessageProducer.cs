31d3e9905f0a48aa82b6c29ece66625e4dbf21a6
raghu <subamraghu11154@gmail.com>
2024-06-11 22:09:02 +0530

    Add project files.

---
+++ Kafka_Producer/MessageProducer.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Kafka_Producer
+{
+    public class MessageProducer
+    {
+        public void produceMessage()
+        {
+            var configuration = new ConfigurationBuilder()
+                       .SetBasePath(AppContext.BaseDirectory)
+                       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                       .Build();
+
+            // Retrieve Kafka configuration
+            var kafkaConfigSection = configuration.GetSection("Kafka");
+            var bootstrapServers = kafkaConfigSection["BootstrapServers"];
+            var topicName = kafkaConfigSection["TopicName"];
+
+            ProducerConfig config = new ProducerConfig { BootstrapServers=bootstrapServers};
+            IProducer<String,String> producer =new ProducerBuilder<String,String>(config).Build();
+            try
+            {
+                String state;
+                while((state=Console.ReadLine())!=null)
+                {
+                    Message<String, String> msg = new Message<string, string> { Key = "newKey", Value = state };
+                    producer.Produce(topicName, msg);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Kafka_Producer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kafka_Producer;
+
+Console.WriteLine("Hello, World!");
+MessageProducer p = new MessageProducer();
+p.produceMessage();
